6c6b2b085484a63d171fcd6a2cf9db9902e8a1c5
AntiBug <75170007+s-h-i-k-h-a-r@users.noreply.github.com>
2023-01-04 11:33:21 +0545

    Update SideBar.jsx

--- src/Scenes/Global/SideBar.jsx
+++ src/Scenes/Global/SideBar.jsx
@@ -104,7 +104,7 @@ const Sidebar = () => {
                   alt="profile-user"
                   width="100px"
                   height="100px"
-                  src={`../../../assets/user.jpg`}
+                  src={`../assets/user.jpg`}
                   style={{ cursor: "pointer", borderRadius: "50%" }}
                 />
               </Box>
